c74f0c1349e71904dac3c50c003a91a379c59fde
Ghatan55 <ghatanhardanni@gmail.com>
2025-03-20 10:58:20 +0700

    Membuat Prosedur Fungsi Input

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -3,3 +3,25 @@ using namespace std;
 
 int arr[20]; // Membuat Array dengan panjang data 20
 int n; // Membuat variable inputan n
+
+void input()
+{ // Procedure Input
+while (true)
+{
+cout << "Masukkan Jumlah Data pada Array : "; // Membuat Inputan jumlah element Array
+cin >> n; // Memanggil variable inputan n 
+
+if (n <= 20)
+{ // Membuat Kondisi n tidak lebih dari 20
+break;
+}
+else
+{
+cout << "\nArray yang anda masukkan maksimal 20 elemen.\n"; // Membuat Pesan jika data lebih dari 20    
+}
+}
+cout << endl; // Membuat jarak per baris program
+cout << "=======================" << endl; // Membuat Tampilan susunan data element array
+cout << "Masukkan Element Array" << endl;
+cout << "=======================" << endl;
+}
