338b7cbfbd8353168ee4db824ca3e624ef4135cf
Ghatan55 <ghatanhardanni@gmail.com>
2025-03-20 11:11:04 +0700

    Membuat Prosedur Fungsi InsertionSort

--- insertionsort.cpp
+++ insertionsort.cpp
@@ -31,3 +31,26 @@ cout << "Data Ke-" << (i + 1) << ": "; // Membuat atau menginputkan nilai data n
 cin >> arr [i]; // Menyimpan nilai data n kedalam array arr 
 }
 }
+
+void InsertionSort()
+{ // Procedure InsertionSort
+
+int temp; // Membuat Variable data temporer atau menyimpan sementara
+int j, i; // Membuat Variable j sebagai penunda
+
+for (i = 1; i <= n - 1; i++)
+{ // Step 1
+
+temp = arr[i]; // step 2
+
+j = i - 1; // Step 3
+
+while (j >= 0 && arr [j] > temp) // Step 4
+{
+arr [j + 1] = arr [j]; // Step 4a
+j--; // Step 4b
+}
+
+arr [j + 1] = temp; // Step 5
+}
+}
